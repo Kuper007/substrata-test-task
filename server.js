89c873eb69fb294e7dbb50781980b0772807a02b
snizhokma <snizhokma@gmail.com>
2024-02-23 19:10:08 +0200

    Add session & /emails endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Koa = require('koa');
+const session = require('koa-session');
 
 const logger = require('koa-morgan');
 const Router = require('koa-router');
@@ -8,11 +9,13 @@ const { koaBody } = require('koa-body');
 
 const fetch = require('isomorphic-fetch');
 const { PublicClientApplication } = require("@azure/msal-node");
+const { Client } = require("@microsoft/microsoft-graph-client");
 
 const clientId = process.env.MS_CLIENT_ID;
 const redirectUri = `http://localhost:${process.env.PORT}/redirect`;
 const tenantId = process.env.MS_TENANT_ID;
 const clientSecret = process.env.MS_CLIENT_SECRET;
+
 
 const koaOptions = {
   origin: true,
@@ -30,9 +33,14 @@ const msalConfig = {
 
 const pca = new PublicClientApplication(msalConfig);
 
-const scopes = ["https://graph.microsoft.com/.default"];
+const scopes = ["https://graph.microsoft.com/Mail.Read"];
 
 const server = new Koa();
+
+
+server.keys = ['initial secret'];
+server.use(session(server));
+
 const router = new Router()
 
 router.get('/', ctx => {
@@ -52,7 +60,6 @@ router.post('/comments', koaBody(), async ctx => {
 })
 
 router.get('/signin', async (ctx) => {
-
   const authCodeUrlParameters = {
     scopes,
     redirectUri,
@@ -75,17 +82,14 @@ router.get('/redirect', async (ctx, next) => {
     redirectUri,
     clientSecret,
   };
-  console.log(clientSecret)
-
 
   try {
-
     const tokenResponse = await pca.acquireTokenByCode(tokenRequest);
-    ctx.state.accessToken = tokenResponse.accessToken;
-    console.log("TOKEN", ctx.state.accessToken)
+    const accessToken = tokenResponse.accessToken;
+    ctx.session.accessToken =  accessToken;
+
 
     await next();
-    ctx.redirect(`http://localhost:${process.env.PORT}/user`)
   } catch (error) {
     console.log(error)
     ctx.throw(401, 'Authentication failed');
@@ -93,22 +97,21 @@ router.get('/redirect', async (ctx, next) => {
 });
 
 // Route to retrieve user data
-router.get('/user', async (ctx) => {
-  console.log("TOKEN", ctx.state.accessToken)
-  const response = await fetch('https://graph.microsoft.com/v1.0/me', {
-    headers: {
-      Authorization: `Bearer ${ctx.state.accessToken}`,
+router.get('/emails', async (ctx, next) => {
+  const accessToken = ctx.session.accessToken;
+  const client = Client.init({
+    authProvider: (done) => {
+      done(null, accessToken);
     },
   });
 
-  if (!response.ok) {
-    ctx.throw(response.status, 'Failed to fetch user data');
-  }
+  const messages = await client.api("/me/messages").top(5).get();
 
-  const userData = await response.json();
-  ctx.body = userData;
+  console.log("messages: ", messages);
+
+  ctx.body = messages;
+  await next();
 });
-
 
 server
   .use(logger('tiny'))
